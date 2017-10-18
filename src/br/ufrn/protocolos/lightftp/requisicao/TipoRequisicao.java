c1abcd89c5c83fd0d046caa7174461b199b97128
Ramon Santos Malaquias <f043684@intrajus.tjrn>
2017-10-18 14:35:19 -0300

    Continuação

--- src/br/ufrn/protocolos/lightftp/requisicao/TipoRequisicao.java
+++ src/br/ufrn/protocolos/lightftp/requisicao/TipoRequisicao.java
@@ -2,7 +2,7 @@ package br.ufrn.protocolos.lightftp.requisicao;
 
 public class TipoRequisicao {
 	public static String SOLICITACAO_LISTA_ARQUIVOS = "SOLICITACAO_LISTA_ARQUIVOS";
-	public static String SOLICITACAO_DOWNLOAD_ARQUIVOS = "SOLICITACAO_DOWNLOAD_ARQUIVOS";
+	public static String SOLICITACAO_DOWNLOAD_ARQUIVO = "SOLICITACAO_DOWNLOAD_ARQUIVO";
 	public static String SOLICITACAO_UPLOAD_ARQUIVO = "SOLICITACAO_UPLOAD_ARQUIVO";
 	public static String FINALIZAR_CONEXAO = "FINALIZAR_CONEXAO";
 }
