3c8631f5a0a531f5191170951e4650b720dec567
Ramon Santos <rsanttos22@gmail.com>
2017-10-23 11:47:48 +0000

    Insercao da validacao da requisicao de lista de arquivos

--- src/br/ufrn/protocolos/lightftp/cliente/main/ClienteMain.java
+++ src/br/ufrn/protocolos/lightftp/cliente/main/ClienteMain.java
@@ -2,19 +2,19 @@ package br.ufrn.protocolos.lightftp.cliente.main;
 
 import java.io.IOException;
 
-import br.ufrn.protocolos.lightftp.cliente.servico.RequisicaoUploadArquivoService;
+import br.ufrn.protocolos.lightftp.cliente.servico.RequisicaoListaArquivosService;
 
 public class ClienteMain {
 	public static void main(String[] args) throws IOException {
 		// RequisicaoDownloadArquivoService requisicaoDownloadArquivoService = new
 		// RequisicaoDownloadArquivoService("casa.jpg");
 		// requisicaoDownloadArquivoService.realizaRequisicao();
-
-		RequisicaoUploadArquivoService requisicaoUploadService = new RequisicaoUploadArquivoService("casa.jpg");
-		requisicaoUploadService.realizaRequisicao();
-
-		// RequisicaoListaArquivosService requisicaoListaArquivosService = new
-		// RequisicaoListaArquivosService();
-		// requisicaoListaArquivosService.realizaRequisicao();
+//
+//		RequisicaoUploadArquivoService requisicaoUploadService = new RequisicaoUploadArquivoService("casa.jpg");
+//		requisicaoUploadService.realizaRequisicao();
+//
+		 RequisicaoListaArquivosService requisicaoListaArquivosService = new
+		 RequisicaoListaArquivosService();
+		 requisicaoListaArquivosService.realizaRequisicao();
 	}
 }
